e540cd3fdc58121d65673621eeec02c4a71b64ef
atjimenez <alexis.jimenez@robinsonsbank.com.ph>
2025-12-02 16:22:33 +0800

    initial commit

---
+++ Activity1Loop.java
@@ -0,0 +1,13 @@
+package Activity1Loop;
+
+public class Activity1Loop {
+	public static void main(String[] args) {
+		int numbers = 0;
+		for (int i = 1; i <= 50; i++)
+		{
+			numbers += i;
+		}
+		
+		System.out.println("Sum = " + numbers);
+	}
+}
