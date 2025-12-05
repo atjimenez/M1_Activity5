ed010020dbf85788c8b3be406ea5251d4f3c3d90
atjimenez <alexis.jimenez@robinsonsbank.com.ph>
2025-12-05 11:09:28 +0800

    M2_ACTIVITY2

---
+++ M2_ACTIVITY2.java
@@ -0,0 +1,12 @@
+package M2_ACTIVITY2;
+
+public class M2_ACTIVITY2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Whiteboard wb = new Whiteboard();
+		int sum = wb.addNumbers(5,3);
+		System.out.println("The total is: " + String.valueOf(sum));
+	}
+
+}
