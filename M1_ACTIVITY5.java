7eec05e52b1701992581d03f06b11df7efa605a9
atjimenez <alexis.jimenez@robinsonsbank.com.ph>
2025-12-05 11:07:23 +0800

    M1_ACTIVITY5

---
+++ M1_ACTIVITY5.java
@@ -0,0 +1,16 @@
+package M1_ACTIVITY5;
+
+public class M1_ACTIVITY5 {
+
+	public static void main(String[] args) {
+		int numbers = 0;
+		for (int i = 1; i <= 50; i++)
+		{
+			numbers += i;
+		}
+		
+		System.out.println("Sum = " + numbers);
+
+	}
+
+}
